f2bc07b1613655a209a80011d18e39a75b0aa427
npjava 2006 <30605905+npjava2006@users.noreply.github.com>
2023-01-15 16:34:12 -0500

    initial junit sample to run junits.

---
+++ tutorial-resources/motors-101/src/test/java/frc/robot/RobotTest.java
@@ -0,0 +1,41 @@
+package frc.robot;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RobotTest {
+
+    //@Mock
+    // TODO Need to integrate Mockito Framework
+    private Robot myRobot;
+
+    @Before
+    public void setUp() throws Exception {
+        myRobot = new Robot();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        myRobot = null;
+    }
+
+    @Test
+    public void testRobotNull() throws Exception{
+       assertNotNull(myRobot);
+    }
+
+    @Test
+    public void testRobotPerioidsNotNull() throws Exception{
+        assertNotNull(myRobot.getPeriod());
+    }
+
+    @Test
+    public void testRobotPerioidsDefault() throws Exception{
+       // System.out.println("actual "+myRobot.getPeriod());
+        double expectedPeriod = 0.02d;
+        assertEquals(expectedPeriod, myRobot.getPeriod(),0);
+    }
+}
